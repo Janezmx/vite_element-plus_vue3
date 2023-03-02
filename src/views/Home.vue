59028d71a7a0de9144b359cd872194e19709f91a
zhangmengxue.jane <zhangmengxue.jane@sh-zhangmx>
2023-03-02 15:04:17 +0800

    单页访问量

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     home
+    <div>本页总阅读量<span id="busuanzi_value_page_pv"></span>次</div>
     <el-button size="small" type="primary" @click="clickMessage">message</el-button>
 </template>
 
